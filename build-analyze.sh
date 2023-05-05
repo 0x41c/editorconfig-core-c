ae71d7156f3b2e271cf3d8acb79828dd3614d18f
0x41c <56287516+0x41c@users.noreply.github.com>
2023-05-04 18:21:59 -0700

    Add sonarlint, connect it to project (yes it's public)

---
+++ build-analyze.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git fetch --unshallow
+
+if [ ! -d "$HOME"/.sonar ]; then
+
+  # Prerequisites
+  export SONAR_SCANNER_VERSION="4.8"
+  mkdir "$HOME"/.sonar
+  
+  # Download build-wrapper
+  curl -sSLo "$HOME"/.sonar/build-wrapper-macosx-x86.zip https://sonarcloud.io/static/cpp/build-wrapper-macosx-x86.zip
+  unzip -o "$HOME"/.sonar/build-wrapper-macosx-x86.zip -d "$HOME"/.sonar/
+  export PATH="$HOME"/.sonar/build-wrapper-macosx-x86:$PATH
+  
+  # Download sonar-scanner
+  curl -sSLo "$HOME"/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx.zip
+  unzip -o "$HOME"/.sonar/sonar-scanner.zip -d "$HOME"/.sonar/
+  export PATH="$HOME"/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx/bin:$PATH
+
+fi
+
+# Run sonar scanner
+sonar-scanner \
+  -Dsonar.login="$SONAR_TOKEN"
